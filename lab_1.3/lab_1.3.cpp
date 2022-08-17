cd9648cf34d7a2c9815a5ff8784265fad54d3101
kokor <kokor@DESKTOP-7U0ABC6>
2022-08-17 13:23:31 +0500

    Добавьте файлы проекта.

---
+++ lab_1.3/lab_1.3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+	float a;
+	float b;
+	float c;
+	float d;
+	float f;
+	float e;
+	cin >> a;
+	cin >> b;
+	c = (pow(a + b, 2));
+	d = (pow(a, 2) + 2 * a * b);
+	f = (pow(b, 2));
+	e = ((c - d) / f);
+	cout << e << endl;
+	return 0;
+}
